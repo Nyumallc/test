db31e6cb609ab25ec180791e70482ba7cc622d90
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 08:40:29 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -9,8 +9,7 @@ window.onload = function() {
 const setLiffClient = () => {
   return (func) => { liff.init({ liffId: LIFF_ID }).then(func) }
 };
-      const userid = profile.userId
-      const displayname =profile.displayName
+
 const getProfile = async (liffClient) => {
   liffClient(() => {
     liff.getProfile().then((profile) => {
@@ -18,7 +17,8 @@ const getProfile = async (liffClient) => {
        document.getElementById('user-name').innerText = profile.displayName
       document.getElementById('icon').src = profile.pictureUrl
       document.getElementById('res').value = profile.userId
-
+      const userid = profile.userId
+      const displayname =profile.displayName
     })
   })
 };
