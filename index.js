af7544ffc9c32d9ad3c7331cc3c28a90ad2a0b1d
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 05:34:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('submit-btn');
 btn.setAttribute('onclick', 'pushButton1()');
 function pushButton1() {
-   document.getElementById('res').innerHTML=resurl;
+   document.getElementById('res').value=resurl;
   }
 
 
