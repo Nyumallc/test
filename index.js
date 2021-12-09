3eb9e6b78e0ad7b328a927cb574b7c4d84c2d4f1
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 02:02:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function urlSubmit(event) {
  const response = await axios.get(`${REQUEST_URL}?userid=${userid()}&displayname=${displayname}&url=${url}&option=${option}`)
  location.reload()
 }
+
+const url = document.getElementById('submit-btn');
 const url = document.getElementById('url').Value;
 const btn = document.getElementById('option').Value;
 
