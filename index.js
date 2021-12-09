47278f78d8ee1ec6d11435d13363b5ba66d01c21
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 03:40:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 let userId = ''
 
-document.getElementById('submit-btn').addEventListener('click' = function() {
+document.getElementById("submit-btn").addEventListener("click" , function() {
   location.replace("https://www.google.com/");
 
-  const response = await axios.get(resurl)
- location.reload()
- fetch(resurl)
-    .then(res => {
-        if(res.ok){
-            return res.json();
-        } else {
-            throw new Error();
-        }
-    })
-    .then(myJson => {
-        console.log(JSON.stringify(myJson));
-    })
-    .catch(err => {
-        console.log(err);
-    })
-});
+//   const response = await axios.get(resurl)
+
+},False);
 
 const resurl=(`${REQUEST_URL}?userid=${userid}&displayname=${displayname}&url=${url}&option=${option}`)
 const btn = document.getElementById('submit-btn');
