4f0a4c7adf77a64f4a9fad6c8a2e6997d35a735b
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 03:27:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let userId = ''
 
-document.getElementById("submit-btn").addEventListener('click' = function() {
+document.getElementById('submit-btn').addEventListener('click' = function() {
   document.getElementById('res').innerHTML =resurl;
   const response = await axios.get(resurl)
  location.reload()
