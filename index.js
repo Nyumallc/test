d4d705b3641f9eb598a17431b5ad1f1810072554
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 02:40:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ const url = document.getElementById('url').Value;
 const option= document.getElementById('option').Value;
 
 btn.addEventListener('click', urlSubmit);
+
+// ページをreload
+function doReload() {
+    window.location.reload(true);
+}
+window.addEventListener('load', function () {
+    // ページ表示完了した5秒後にリロード
+    setTimeout(doReloadNoCache, 10);
+});
