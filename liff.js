14ade9deb6d9fadcd7cf014b24eb7d6b735b0e1f
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 08:46:20 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -16,9 +16,8 @@ const getProfile = async (liffClient) => {
       // 02. プロフィールにユーザー名を表示してみよう
        document.getElementById('user-name').innerText = profile.displayName
       document.getElementById('icon').src = profile.pictureUrl
-      document.getElementById('res').value = profile.userId
-      const userid = profile.userId
-      const displayname =profile.displayName
+      var userid = profile.userId
+      var displayname =profile.displayName
     })
   })
 };
