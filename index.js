91891817a65a6fe9645ae2337c1a264146e15b9a
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 13:29:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function pushButton1() {
     let url = document.getElementById("url");
     let urltext = url.value;
     let option= document.getElementById("option");
-    let optiontext= option.innerHTML;
+    let optiontext= option.value;
     
     let resurl=(`${REQUEST_URL}?&userid=${userid}&displayname=${displayname}&url=${urltext}&option=${optiontext}`)
 
