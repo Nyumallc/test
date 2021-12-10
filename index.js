d4696c8f02fceaf113c82f37504f67061ececd15
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 12:35:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const url = document.getElementById("url");
-const urltext = url.value;
-const option= document.getElementById("option");
-const optiontext= option.Value;
-const btn = document.getElementById("submit-btn");
-// var resurl=(`${REQUEST_URL}?userid=${profile.userId}&displayname=${displayname}&url=${url}&option=${option}`);
-var resurl = REQUEST_URL + '?userid=' + userid + '&displayname=' + displayname + '&url=' + url + '&option=' + option;
-
-
 btn.setAttribute('onclick', 'pushButton1()');
 function pushButton1() {
+    let url = document.getElementById("url");
+    let urltext = url.value;
+    let option= document.getElementById("option");
+    let optiontext= option.Value;
+    let btn = document.getElementById("submit-btn");
+    let resurl=(`${REQUEST_URL}?&userid=${userid}&displayname=${displayname}&url=${url}&option=${option}`)
+
          alert(resurl);
         const res = fetch(resurl);
         console.log(res);
