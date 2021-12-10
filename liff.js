f83daacf5b26f360b480a5952c2f52f8dd776ab9
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 22:11:40 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -12,7 +12,7 @@ const setLiffClient = () => {
 
 const getProfile = async (liffClient) => {
   liffClient(() => {
-    liff.getProfile().then((profile) => {
+    await  liff.getProfile().then((profile) => {
       // 02. プロフィールにユーザー名を表示してみよう
       document.getElementById('user-name').innerText = profile.displayName
       document.getElementById('icon').src = profile.pictureUrl
