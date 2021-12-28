bbd7e1cf828391be0c169c2052404d1ea5ad2bb8
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-28 18:15:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,3 @@ function pushButton1() {
             // document.getElementById('log').value += 'sendMessagesText()=' + error + '\n';
           });
     };
-
-
-btn2 = document.getElementById("rireki-btn");
-btn2.setAttribute('onclick', 'pushButton2()');
-function pushButton2() {
-    let user_id = document.getElementById("user_id");
-    let userid = user_id.innerHTML;
-    let resurl=(`https://share.streamlit.io/nyumallc/streamlit_db/main/streamlit_db.py?userid=${userid}`)
