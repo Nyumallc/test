8d4ea686f582552bcf6f1cb41b7ab2c5e0a0bec5
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-03-06 19:13:26 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,24 +1,23 @@
-window.onload = function() {
-  const liffClient = setLiffClient()
-
-  // setup をする
-  liffClient(() => { if(!liff.isLoggedIn())liff.login(); })
-  getProfile(liffClient)
-};
-
-const setLiffClient = () => {
-  return (func) => { liff.init({ liffId: LIFF_ID }).then(func) }
-};
-
-const getProfile = async (liffClient) => {
-  liffClient(() => {
-     liff.getProfile().then((profile) => {
-      // 02. プロフィールにユーザー名を表示してみよう
-      document.getElementById('user-name').innerText = profile.displayName
-      document.getElementById('icon').src = profile.pictureUrl
-      document.getElementById('user_id').innerText = profile.userId
-      let userid = profile.userId
-      let displayname =profile.displayName
+window.addEventListener('load', async () => {
+    const liffClient = await setLiffClient()
+  
+    liffClient(() => { if(!liff.isLoggedIn())liff.login(); })
+    getProfile(liffClient)
+  });
+  
+  const setLiffClient = () => {
+    return (func) => { liff.init({ liffId: LIFF_ID }).then(func) }
+  }
+  
+  const getProfile = async (liffClient) => {
+    liffClient(() => {
+      liff.getProfile().then((profile) => {
+        // 02. プロフィールにユーザー名を表示してみよう
+        document.getElementById('user-name').innerText = profile.displayName
+        document.getElementById('icon').src = profile.pictureUrl
+        document.getElementById('user_id').innerText = profile.userId
+        let userid = profile.userId
+        let displayname =profile.displayName
+      })
     })
-  })
-};
+  };
