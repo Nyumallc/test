22e4aaaf6578265ed92adf013a893000440b6165
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-03-06 19:58:00 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,9 +1,19 @@
 window.addEventListener('load', async () => {
+    const liffClient = await setLiffClient()
+  
+    liffClient(() => { if(!liff.isLoggedIn())liff.login(); })
+    getProfile(liffClient)
+  });
+  
+  const setLiffClient = () => {
+    return (func) => { liff.init({ liffId: LIFF_ID }).then(func) }
+  };
+
   liff
   .init({
     liffId: LIFF_ID, // Use own liffId
   })
   .then(() => {
-    const idToken = liff.getIDToken();
+    const idToken = liff.getDecodedIDToken();
     console.log(idToken);
   });
