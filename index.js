7d2f4c23c1d9bfd8270eb9aade11ace39df290ae
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-03-30 00:14:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var p_titie = params.get('p_titie');
 
 
 btn1 = document.getElementById("submit-btn");
-btn1.setAttribute('onclick', 'pushButton1(this.id)');
+btn1.setAttribute('onclick', 'pushButton1()');
 
 function pushButton1() {
     let name = document.getElementById("user-name");
@@ -13,7 +13,7 @@ function pushButton1() {
     // let key = document.getElementById("keyword");
     // let key = key.value;
 
-    let resurl=(`https://script.google.com/macros/s/AKfycbzPDS6SjcPf_Ud5a8FVeun7V2drrkrbV41YX-02KLr7vQKgV9eqZftrSHk1_Uh9sTQ/exec?userid=${userid}&displayname=${displayname}&p_title=${p_title}`)
+    let resurl=(`https://script.google.com/macros/s/AKfycbzPDS6SjcPf_Ud5a8FVeun7V2drrkrbV41YX-02KLr7vQKgV9eqZftrSHk1_Uh9sTQ/exec?&userid=${userid}&displayname=${displayname}&p_title=${p_title}`)
     const res = fetch(resurl);
     
   };
